f9b530b6c099b912cb676ea494760f3d63153942
Dexin.Zhang <dexin.zhang@noaa.gov>
2019-06-18 15:22:17 +0000

    Unified build (20190618) script/makefile sigio v2.0.1

--- Conf/Sigio_intel_General.sh
+++ Conf/Sigio_intel_General.sh
@@ -14,7 +14,7 @@
 
  ANCHORDIR=..
  export COMP=ips
- export SIGIO_VER=v2.1.0
+ export SIGIO_VER=v2.0.1
  export SIGIO_SRC=
  export SIGIO_INC4=$ANCHORDIR/include/sigio_${SIGIO_VER}_4
  export SIGIO_LIB4=$ANCHORDIR/libsigio_${SIGIO_VER}_4.a
